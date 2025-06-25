e014f60e0a4baf400954e2efe83ec6552a0abee3
Anastasiia-Domanska <akvadelf24@gmail.com>
2025-06-25 21:15:32 +0200

    Add classwork14

--- src/test/java/com_ait_qa60/FindElementTests.java
+++ src/test/java/com_ait_qa60/FindElementTests.java
@@ -63,5 +63,40 @@ public class FindElementTests {
         driver.findElement(By.cssSelector("[href^='/log']"));
 
         driver.findElement(By.cssSelector("[href$='search']"));
+
+        driver.findElement(By.cssSelector("input#city"));
+
+        driver.findElement(By.cssSelector("div.search-card"));
+
+        driver.findElement(By.cssSelector("input#city[type='text']"));
+    }
+    @Test
+    public void findElementByXpath(){
+        driver.findElement(By.xpath("//h1"));
+
+        //driver.findElement(By.xpath("//input[@id='city']"));
+        // or
+        driver.findElement(By.xpath("//*[@id='city']"));
+
+        driver.findElement(By.xpath("//div[@class='header']"));
+
+        driver.findElement(By.xpath("//h2[contains(.,'Yalla')]"));
+
+        //driver.findElement(By.xpath("//h2[text()='Type your data and hit Yalla!']"));
+        driver.findElement(By.xpath("//h2[.='Type your data and hit Yalla!']"));
+
+        driver.findElement(By.xpath("//label[starts-with(@for,'ci')]"));
+
+        driver.findElement(By.xpath("//a[@class='navigation-link']/.."));
+
+        driver.findElement(By.xpath("//h1/parent::*"));
+        driver.findElement(By.xpath("//h1/parent::div"));
+        driver.findElement(By.xpath("//h1/.."));
+
+        driver.findElement(By.xpath("//h1/ancestor::*")); // all
+        driver.findElement(By.xpath("//h1/ancestor::div")); // 2 options
+        driver.findElement(By.xpath("//h1/ancestor::div[2]")); // 1 option
+        driver.findElement(By.xpath("//h1/following-sibling::form"));
+        driver.findElement(By.xpath("//h2/preceding-sibling::*"));
     }
 }
